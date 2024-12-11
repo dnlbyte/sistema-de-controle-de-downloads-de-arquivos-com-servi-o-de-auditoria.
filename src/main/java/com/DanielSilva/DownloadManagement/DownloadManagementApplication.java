9c386277cddee91bda10e7203a417e89d79db330
Daniel Silva De Lima <daniel_lma@hotmail.com>
2024-12-11 14:49:25 -0300

    second commit

---
+++ src/main/java/com/DanielSilva/DownloadManagement/DownloadManagementApplication.java
@@ -0,0 +1,13 @@
+package com.DanielSilva.DownloadManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DanielSilva/DownloadManagement/DownloadManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DanielSilva.DownloadManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
